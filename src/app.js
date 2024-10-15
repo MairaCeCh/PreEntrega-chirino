4200dcba9035d1efc8d312e40c50b2d8c0ec4c10
maira chirino <112666191+MairaCeCh@users.noreply.github.com>
2024-10-15 17:47:10 -0300

    fin pre entrega 1

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import { nanoid } from "nanoid";
 
 const app = express();
 let storeProducts = [];
+let carts = [];
 
 async function fetchProducts(filePath) {
   try {
@@ -16,11 +17,22 @@ async function fetchProducts(filePath) {
 }
 fetchProducts("./src/products.json");
 
+async function fetchCarts(filePath) {
+  try {
+    const data = await fs.promises.readFile(filePath, "utf-8");
+    carts = JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading JSON file:", error);
+  }
+}
+
+fetchCarts("./src/carts.JSON");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const httpServer = app.listen(config.PORT, () => {
-  console.log(`gatito feliz ${config.PORT}`);
+  console.log(`todo ok en el puerto ${config.PORT}`);
 });
 ///////productos
 
@@ -119,5 +131,58 @@ app.delete("/api/products/:id", async (req, res) => {
 /////////carrito
 
 app.get("/api/carts", (req, res) => {
-  res.status(200).send("carrito");
+  if (carts.length > 0) {
+    res.status(200).send("carrito ok" + JSON.stringify(carts));
+  } else {
+    res.status(404).send("No hay carritos disponibles");
+  }
+});
+
+app.post("/api/carts", async (req, res) => {
+  const newCart = {
+    id: nanoid(10),
+    products: [],
+  };
+
+  carts.push(newCart);
+
+  await fs.promises.writeFile(
+    "./src/carts.json",
+    JSON.stringify(carts, null, 2)
+  );
+  res.status(201).send({ error: null, data: newCart });
+});
+
+app.get("/api/carts/:cid", (req, res) => {
+  const cid = req.params.cid;
+  const cart = carts.find((element) => element.id == cid);
+
+  if (cart) {
+    return res.status(200).json(cart);
+  }
+
+  return res.status(404).send("Carrito no encontrado");
+});
+
+app.post("/api/carts/:cid/products/:pid", async (req, res) => {
+  const cid = req.params.cid;
+  const pid = req.params.pid;
+
+  const cart = carts.find((element) => element.id == cid);
+  if (!cart) {
+    return res.status(404).send({ error: "Carrito no encontrado", data: [] });
+  }
+
+  const productInCart = cart.products.find((p) => p.product == pid);
+  if (productInCart) {
+    productInCart.quantity += 1;
+  } else {
+    cart.products.push({ product: pid, quantity: 1 });
+  }
+
+  await fs.promises.writeFile(
+    "./src/carts.json",
+    JSON.stringify(carts, null, 2)
+  );
+  res.status(201).send({ cart });
 });
