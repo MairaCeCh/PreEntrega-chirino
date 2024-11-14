cee6eca718631e8dffcad7a3de006d99b8b8383d
mayra <112666191+MairaCeCh@users.noreply.github.com>
2024-11-14 11:01:12 -0300

    agregando handlebars

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,71 +1,121 @@
-import { Router } from 'express';
-import { nanoid } from 'nanoid';
-import fs from 'fs';
+import { Router } from "express";
 
+import CartController from "../dao/cart.controller.js";
 
 const router = Router();
-let storeCarts = [];
-
-
-async function fetchCarts(filePath) {
-    try {
-        const data = await fs.promises.readFile(filePath, 'utf-8');
-        storeCarts = JSON.parse(data);
-    } catch (error) {
-        console.error('Error reading JSON file:', error);
-    }
-}
-
-fetchCarts('./src/carts.json');
+const controller = new CartController();
 
 let storeProducts = [];
 
-async function fetchProducts(filePath) {
-    try {
-        const data = await fs.promises.readFile(filePath, 'utf-8');
-        storeProducts = JSON.parse(data);
-    } catch (error) {
-        console.error('Error reading JSON file:', error);
-    }
+// async function fetchProducts(filePath) {
+//   try {
+//     const data = await fs.promises.readFile(filePath, "utf-8");
+//     storeProducts = JSON.parse(data);
+//   } catch (error) {
+//     console.error("Error reading JSON file:", error);
+//   }
+// }
+
+// fetchProducts("./src/products.json");
+
+router.get('/', async (req, res)=> {
+    const process = await controller.get()
+    res.status(200).send({error: null, data: process})
+})
+
+router.get("/:cid", async(req, res) => {
+  const cartID = req.params.cid;
+  try {
+    const cart = await controller.getOne({_id:cartID});
+    res.send({cart});
+} catch (error) {
+    console.error('Error al obtener productos:', error);
+    res.status(500).render('error', { message: 'Error al cargar los productos' });
 }
+});
 
-fetchProducts('./src/products.json');
 
-router.get('/:cid', (req, res) => {
-    const cartID = req.params.cid; 
-    const cart = storeCarts.find(element => element.id == cartID);
+router.post("/", async (req, res) => {
+  const process = await controller.add({products: []});
+  res.status(201).send({ error: null, data: process });
+});
+
+
+router.post("/:cid/products/:pid", async (req, res) => {
+    const { cid, pid } = req.params;
+    const cart = await controller.fOne({ _id: cid });
+    
     if (!cart) {
-        return res.status(404).send({ error: "Cart not found", data: null });
+        console.log('Carrito no encontrado:', cart);
+        return res.status(406).send({ error: "Carrito no encontrado" });
     }
     
-    res.status(200).send({ data: cart.products });
-});
+    const productInCart = cart.products.find((product) => product._id.toString() === pid);
 
-router.post('/', async (req, res) => {
-    const newCart = { id: nanoid(10), products: [] };
-    storeCarts.push(newCart);
-    await fs.promises.writeFile('./src/carts.json', JSON.stringify(storeCarts, null, 2));
-    res.status(201).send({ error: null, data: newCart });
-});
-
-router.post('/:cid/products/:pid', async (req, res) => {
-    const { cid, pid } = req.params;
-    const cart = storeCarts.find(c => c.id == cid);
-    if (!cart) {
-        return res.status(404).send({ error: 'Carrito no encontrado' });
-    }
-    const productExists = storeProducts.some(p => p.id == pid);
-    if (!productExists) {
-        return res.status(404).send({ error: 'Producto no encontrado' });
-    }
-    const productInCart = cart.products.find(p => p.product == pid);
     if (productInCart) {
         productInCart.quantity += 1;
     } else {
-        cart.products.push({ product: pid, quantity: 1 });
+        cart.products.push({ _id: pid, quantity: 1 });
     }
-    await fs.promises.writeFile('./src/carts.json', JSON.stringify(storeCarts, null, 2));
-    res.status(200).send({ error: null, data: cart });
+    
+    
+    const cartToUpdate = {
+        _id: cart._id.toString(),
+        products: cart.products
+    };
+    
+    const updatedCart = await controller.addProduct(cartToUpdate);
+    
+    res.status(200).send({ error: null, data: updatedCart });
+});
+
+router.delete('/:cid/products/:pid', async (req, res) => {
+    const { cid, pid } = req.params;
+
+    try {
+   
+        const cart = await controller.fOne({ _id: cid });
+        if (!cart) {
+            return res.status(404).send({ error: 'Carrito no encontrado' });
+        }
+
+        const updatedProducts = cart.products.filter(product => product._id.toString() !== pid);
+
+        const updatedCart = await controller.update(
+            { _id: cid }, 
+            { products: updatedProducts }, 
+            { new: true } 
+        );
+
+        res.status(200).send({ error: null, data: updatedCart });
+    } catch (err) {
+        console.error("Error al eliminar el producto del carrito:", err);
+        res.status(500).send({ error: "Error interno del servidor" });
+    }
+});
+
+router.delete('/:cid', async (req, res) => {
+    const { cid } = req.params;
+
+    try {
+        const cart = await controller.fOne({ _id: cid });
+        console.log('cart:', cart);
+        
+        if (!cart) {
+            return res.status(404).send({ error: 'Carrito no encontrado' });
+        }
+
+        const updatedCart = await controller.update(
+            { _id: cid }, 
+            { products: [] }, 
+            { new: true } 
+        );
+
+        res.status(200).send({ error: null, data: updatedCart });
+    } catch (err) {
+        console.error("Error al eliminar todos los productos del carrito:", err);
+        res.status(500).send({ error: "Error interno del servidor" });
+    }
 });
 
 export default router;
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,30 +1,130 @@
 import { Router } from "express";
-//import { promises as fs } from 'fs'
+
+import ProductController from "../dao/product.controller.js";
+import CartController from "../dao/cart.controller.js";
+
+const CaController = new CartController()
+const ProController = new ProductController()
 
 const router = Router();
 
-// async function fetchProducts(filePath) {
-//     try {
-//         const data = await fs.readFile(filePath, 'utf-8');
-//         const products = JSON.parse(data);
-//         return products;
-//     } catch (error) {
-//         console.error('Error al leer el archivo JSON:', error);
-//         return [];
-//     }
-// }
-
-router.get('/', async (req, res) => {
+router.get('/products', async (req, res) => {
     try {
-        const products = await fetchProducts('./src/products.json');
-     
-        res.render('home', { products });
+        let url = 'http://localhost:8080/api/products';
+        const queryParams = new URLSearchParams(req.query);     
+        if (queryParams.toString()) {
+            url += `?${queryParams.toString()}`; 
+        }
+        console.log( 'url:',url )
+        const response = await fetch(url);
+        const products = await response.json();
+        res.render('home', { products: products.data });
     } catch (error) {
         console.error('Error al obtener productos:', error);
         res.status(500).render('error', { message: 'Error al cargar los productos' });
     }
 });
 
+router.get('/products/:pid', async (req, res) => {
+    const pid = req.params.pid
+    try {
+        console.log('pid:',pid)
+       
+        const product = await ProController.getOne({_id:pid});
+        res.render('product', {product});
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+        res.status(500).render('error', { message: 'Error al cargar los productos' });
+    }
+});
+
+router.get('/:cid/products/:pid', async (req, res) => {
+    const pid = req.params.pid
+    const cid = req.params.cid
+    try {
+
+        const product = await ProController.getOne({_id:pid});
+        res.render('product', {product, cid});
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+        res.status(500).render('error', { message: 'Error al cargar los productos' });
+    }
+});
+
+router.get('/products/paginated/:pg',  async(req, res) => {
+    const pg = req.params.pg;
+    const products = await ProController.getPaginated(pg)
+    res.status(200).render('home', { products });
+});
+
+router.get('/:cid/products', async (req, res) => {
+    const cid = req.params.cid;
+
+    try {
+
+        let url = 'http://localhost:8080/api/products';
+
+        const queryParams = new URLSearchParams(req.query);
+       
+        if (queryParams.toString()) {
+            url += `?${queryParams.toString()}`; 
+        }
+        const response = await fetch(url);
+        const products = await response.json();
+        res.render('home', { products: products.data, cid });
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+        res.status(500).render('error', { message: 'Error al cargar los productos' });
+    }
+});
+
+router.get('/realTimeProducts', (req,res)=> {
+    res.status(200).render('realTimeProducts')
+})
+
+router.get('/realTimeProducts/paginated/:pg', (req,res)=> {
+    const pg = req.params.pg
+    res.status(200).render('realTimeProducts', {pg})
+})
+
+
+
+router.get('/carts', async (req, res)=> {
+
+    try {
+        const response = await fetch('http://localhost:8080/api/carts')
+        const carts = await response.json();
+      //  console.log('carts:',carts)
+        res.render('carts', {carts: carts.data});
+    } catch (error) {
+        console.error('Error al obtener productos:', error);
+        res.status(500).render('error', { message: 'Error al cargar los productos' });
+    }
+
+    })
+
+router.get('/carts/:cid', async(req, res)=> {
+    const cid = req.params.cid
+    const cart = await CaController.getOne({_id: cid})
+    res.status(200).render('cart', {cart})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////
 router.get('/realTimeProducts', (req,res)=> {
     res.status(200).render('realTimeProducts')
 })
